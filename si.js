6058a7a78f0584d25eea1fde6da89898b8eac405
Saumitra3051 <102472373+Saumitra3051@users.noreply.github.com>
2022-03-29 22:41:47 +0530

    Add files via upload

---
+++ si.js
@@ -0,0 +1,32 @@
+function Compute(){
+    // simple interest 
+    let a=document.getElementById("amount").value;
+    let b=document.getElementById("rate").value;
+    let c=document.getElementById("time").value;
+
+    let si=a*b*c/100
+    var year = new Date().getFullYear()+parseInt(c);
+
+    
+    let s=document.getElementById("a").innerHTML="If you deposit "+a 
+    let d=document.getElementById("b").innerHTML="at an interest rate of"+b
+    let r=document.getElementById("si").innerHTML="You will receive an amount of "+si
+    let final=document.getElementById("c").innerHTML="in the year "+year
+//    alert funtion
+    if(a==""){
+        alert("please enter Amount");
+        
+    }
+    
+    if(b==""){
+        alert("please enter Rate");
+
+    }
+   
+    if(c==""){
+        alert("please enter Time");
+    } 
+
+   
+    
+}
